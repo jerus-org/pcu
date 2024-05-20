06dc3fe7356b7b80bb3881795628024bf850f697
Jeremiah Russell <jerry@jrussell.ie>
2024-05-20 16:25:55 +0100

    feat: add calculate_kind_and_description for pr_title
    * feat(Cargo.toml): add keep-a-changelog dependency
    * fix(pr_title.rs): refactor PrTitle struct to include kind and description fields
    * feat(pr_title.rs): add method to calculate kind and description based on commit type and scope
    * feat(pr_title.rs): add methods to retrieve kind and description

--- src/pr_title.rs
+++ src/pr_title.rs
@@ -1,9 +1,13 @@
+use keep_a_changelog::ChangeKind;
+
 #[derive(Debug)]
 pub struct PrTitle {
     pub title: String,
     pub commit_type: Option<String>,
     pub commit_scope: Option<String>,
     pub commit_breaking: bool,
+    kind: Option<ChangeKind>,
+    description: String,
 }
 
 impl PrTitle {
@@ -27,6 +31,8 @@ impl PrTitle {
                 commit_type,
                 commit_scope,
                 commit_breaking,
+                kind: None,
+                description: String::new(),
             }
         } else {
             Self {
@@ -34,7 +40,74 @@ impl PrTitle {
                 commit_type: None,
                 commit_scope: None,
                 commit_breaking: false,
+                kind: None,
+                description: String::new(),
             }
+        }
+    }
+
+    fn calculate_kind_and_description(&mut self) {
+        let mut kind = ChangeKind::Changed;
+        let mut description = self.title.clone();
+
+        if let Some(commit_type) = &self.commit_type {
+            match commit_type.as_str() {
+                "feat" => kind = ChangeKind::Added,
+                "fix" => kind = ChangeKind::Fixed,
+                _ => {
+                    kind = ChangeKind::Changed;
+                    description =
+                        format!("{}: {}", self.commit_type.as_ref().unwrap(), description);
+                }
+            }
+        }
+
+        if let Some(commit_scope) = &self.commit_scope {
+            match commit_scope.as_str() {
+                "security" => {
+                    kind = ChangeKind::Security;
+                    description = format!("Security: {}", self.title);
+                }
+                "deps" => {
+                    kind = ChangeKind::Security;
+                    description = format!("Dependencies: {}", self.title);
+                }
+                "remove" => {
+                    kind = ChangeKind::Removed;
+                    description = format!("Removed: {}", self.title);
+                }
+                "deprecate" => {
+                    kind = ChangeKind::Deprecated;
+                    description = format!("Deprecated: {}", self.title);
+                }
+                _ => {
+                    kind = ChangeKind::Changed;
+                    description =
+                        format!("{}: ({})", description, self.commit_scope.as_ref().unwrap());
+                }
+            }
+        }
+
+        if self.commit_breaking {
+            description = format!("BREAKING: {}", description);
+        }
+
+        self.kind = Some(kind);
+        self.description = description;
+    }
+
+    fn kind(&self) -> ChangeKind {
+        match &self.kind {
+            Some(kind) => kind.clone(),
+            None => ChangeKind::Changed,
+        }
+    }
+
+    fn description(&self) -> String {
+        if self.description.as_str() == "" {
+            self.title.clone()
+        } else {
+            self.description.clone()
         }
     }
 }
