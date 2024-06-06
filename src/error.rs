8db18186681db2271a668061c56a323a8f5e3dd6
Jeremiah Russell <jerry@jrussell.ie>
2024-06-06 07:49:16 +0100

    feat: sign the commit using gpg
    * chore(client.rs): refactor code to improve readability and maintainability
    * fix(client.rs): fix unused constant warning
    * feat(client.rs): add support for signing commits with GPG
    * fix(error.rs): add new error variants for signing process

--- src/error.rs
+++ src/error.rs
@@ -3,6 +3,16 @@ use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum Error {
+    /// Unable to acquire the child process' standard input to write the commit data for signing
+    #[error("Failed to acquire standard input handler")]
+    Stdin,
+
+    /// Unable to retrieve the signed data from the child process
+    #[error("Failed to get output of signing process call: {0}")]
+    Stdout(String),
+
+    #[error("{0}")]
+    GpgError(String),
     #[error("Environment variable PCU_BRANCH not set")]
     EnvVarBranchNotSet,
     #[error("Environment specified in PCU_BRANCH not set")]
@@ -27,4 +37,8 @@ pub enum Error {
     Git2(#[from] git2::Error),
     #[error("env var says: {0:?}")]
     EnvVar(#[from] env::VarError),
+    #[error("io error says: {0:?}")]
+    IOError(#[from] std::io::Error),
+    #[error("utf8 error says: {0:?}")]
+    Utf8Error(#[from] std::str::Utf8Error),
 }
