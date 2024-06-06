ae2f1c786ccac84cd05604da250720716998dc71
Jeremiah Russell <jerry@jrussell.ie>
2024-06-06 17:39:57 +0100

    feat: clap to support logging verbosity
    * chore(main.rs): refactor main.rs to use clap for command line argument parsing
    * feat(main.rs): add support for logging configuration using env_logger

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,23 @@
 use std::fs;
 
+use clap::Parser;
 use keep_a_changelog::ChangeKind;
 use pcu_lib::{Client, Error};
 
 use eyre::Result;
 
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Cli {
+    #[clap(flatten)]
+    logging: clap_verbosity_flag::Verbosity,
+}
+
 #[tokio::main]
 async fn main() -> Result<()> {
-    env_logger::init();
+    let args = Cli::parse();
+    let mut builder = get_logging(args.logging.log_level_filter());
+    builder.init();
 
     let client = match Client::new().await {
         Ok(client) => client,
@@ -91,4 +101,14 @@ fn print_changelog(changelog_path: &str) {
         }
         println!("----------------------------\n",);
     };
+}
+
+fn get_logging(level: log::LevelFilter) -> env_logger::Builder {
+    let mut builder = env_logger::Builder::new();
+
+    builder.filter(None, level);
+
+    builder.format_timestamp_secs().format_module_path(false);
+
+    builder
 }
