7b282881b63b6e13df4df61519e5e15c8b2a6922
Jeremiah Russell <jerry@jrussell.ie>
2024-06-07 16:02:51 +0100

    fix: sign not required and defaults to gpg
    * feat(main.rs): make sign argument optional in Cli struct
    * fix(main.rs): set default value for sign if not provided
    * fix(main.rs): correct the logging level assignment in get_logging function

--- src/main.rs
+++ src/main.rs
@@ -23,7 +23,7 @@ struct Cli {
     #[clap(flatten)]
     logging: clap_verbosity_flag::Verbosity,
     #[clap(short, long)]
-    sign: Sign,
+    sign: Option<Sign>,
 }
 
 #[tokio::main]
@@ -49,7 +49,13 @@ async fn main() -> Result<()> {
         client.branch()
     );
 
-    match run_update(client, args.sign).await {
+    let sign = if let Some(sign) = args.sign {
+        sign
+    } else {
+        Sign::default()
+    };
+
+    match run_update(client, sign).await {
         Ok(_) => log::info!("Changelog updated!"),
         Err(e) => log::error!("Error updating changelog: {e}"),
     };
@@ -129,7 +135,7 @@ fn get_logging(level: log::LevelFilter) -> env_logger::Builder {
     println!("Making builder with Log level: {level}");
 
     let env = Env::new()
-        .filter_or(LOG_ENV_VAR, level.to_string())
+        .filter_or(LOG_ENV_VAR, "level.to_string()")
         .write_style_or(LOG_STYLE_ENV_VAR, "auto");
 
     let mut builder = env_logger::Builder::from_env(env);
