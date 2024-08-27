b79da96d2cd9c5469250c048008a4a451ce83b47
Jeremiah Russell <jerry@jrussell.ie>
2024-08-27 15:54:27 +0100

    feat: add label support for pull requests
    * feat(graphql): add label.rs for handling labels on PRs
    * refactor(graphql): split GraphQL trait into GraphQLLabel and GraphQLPR
    * feat(git_ops.rs): implement PR labeling and sorting in rebase_next_pr method

--- src/lib/client/graphql/mod.rs
+++ src/lib/client/graphql/mod.rs
@@ -1,12 +1,7 @@
+mod label;
 mod pr_title;
 mod prs;
 
+pub(crate) use label::GraphQLLabel;
 pub(crate) use pr_title::get_pull_request_title;
-use prs::PrItem;
-
-use crate::Error;
-
-pub(crate) trait GraphQL {
-    #[allow(async_fn_in_trait)]
-    async fn get_open_pull_requests(&self) -> Result<Vec<PrItem>, Error>;
-}
+pub(crate) use prs::GraphQLPR;
