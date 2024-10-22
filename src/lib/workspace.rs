71335af258eef3178f95393614f1484d2147debc
Jeremiah Russell <jerry@jrussell.ie>
2024-10-22 17:54:15 +0100

    feat(workspace): add method to retrieve packages from workspace manifest

--- src/lib/workspace.rs
+++ src/lib/workspace.rs
@@ -14,4 +14,36 @@ impl Workspace {
 
         Ok(Self { manifest })
     }
+
+    pub fn packages(&self) -> Option<Vec<Package>> {
+        if let Some(workspace) = &self.manifest.workspace {
+            let members = &workspace.members;
+
+            let mut packages = Vec::new();
+
+            for member in members {
+                let member_file = format!("./{member}/Cargo.toml");
+                let path = Path::new(&member_file);
+                let manifest = Manifest::from_path(path).unwrap();
+                if let Some(package) = manifest.package {
+                    let name = package.name;
+                    let version = package.version.get().unwrap().to_string();
+
+                    let package = Package { name, version };
+
+                    packages.push(package);
+                }
+            }
+
+            return Some(packages);
+        }
+
+        None
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct Package {
+    pub name: String,
+    pub version: String,
 }
