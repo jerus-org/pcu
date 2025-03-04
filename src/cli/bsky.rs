559ab611fec7801efff53448b8311c65c70eb99c
Jeremiah Russell <jerry@jrussell.ie>
2025-03-04 06:45:30 +0000

    ✨ feat(cli): add functionality to fetch and log changed files
    - integrate GitHub REST API to fetch the latest release and compare commits
    - log the list of changed files from the commit comparison

--- src/cli/bsky.rs
+++ src/cli/bsky.rs
@@ -9,6 +9,30 @@ pub struct Bsky {}
 
 impl Bsky {
     pub async fn run(&self) -> Result<CIExit> {
+        let client = Commands::Bsky(self.clone()).get_client().await?;
+
+        let release = client
+            .github_rest
+            .repos
+            .get_latest_release(client.owner(), client.repo())
+            .send()
+            .await?;
+
+        let compare = client
+            .github_rest
+            .repos
+            .compare_commits(client.owner(), client.repo(), release.tag_name)
+            .send()
+            .await?;
+
+        if let Some(files) = compare.files {
+            log::info!("Files: {files:?}");
+            let changed_files = files.iter().map(|f| f.filename.clone()).collect::<Vec<_>>();
+            log::info!("Changed files: {changed_files:#?}");
+        }
+
+        // TODO: Get the list of blogs from the config
+
         // TODO: Identify blogs that have changed
         // TODO: For each blog, extract the title, description, and tags
         // TODO: For each blog, create a Bluesky post
