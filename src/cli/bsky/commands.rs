7a089a1b6b9cfec5b6041cbbd04c281cf4c6656d
Jeremiah Russell <jerry@jrussell.ie>
2025-03-10 15:23:44 +0000

    ♻️ refactor(cli): rename command from build to draft
    - change command name from "Build" to "Draft" for clarity
    - update Display trait implementation to reflect command name change

--- src/cli/bsky/commands.rs
+++ src/cli/bsky/commands.rs
@@ -4,14 +4,14 @@ use clap::Subcommand;
 
 #[derive(Debug, Subcommand, Clone)]
 pub enum Cmd {
-    Build,
+    Draft,
     Post,
 }
 
 impl Display for Cmd {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Cmd::Build => write!(f, "build"),
+            Cmd::Draft => write!(f, "draft"),
             Cmd::Post => write!(f, "post"),
         }
     }
