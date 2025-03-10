1362ee4751f724319d78a594927e8c20e2cd33ba
Jeremiah Russell <jerry@jrussell.ie>
2025-03-10 13:34:05 +0000

    ♻️ refactor(bsky): simplify poster implementation
    - remove unused SiteConfig struct and related logic
    - refactor Poster struct to use bsky_posts instead of blog_posts
    - simplify new function to use default implementation
    - update load function to load posts from directory
    - refine post_to_bluesky function to handle posting logic
    
    🐛 fix(bsky): improve error handling for Bluesky posting
    
    - replace color_eyre error handling with custom Error type
    - enhance error messages for missing identifier and password
    - add error handling for Bluesky login and record creation

--- src/cli/bsky/poster.rs
+++ src/cli/bsky/poster.rs
@@ -1,127 +1,80 @@
-use std::fs;
+use std::{fmt::Display, fs, io::BufReader, path::Path};
 
 use bsky_sdk::{
-    agent::config::Config,
-    api::{app::bsky::feed::post::RecordData, types::string::Datetime},
-    rich_text::RichText,
-    BskyAgent,
+    agent::config::Config as BskyConfig, api::app::bsky::feed::post::RecordData, BskyAgent,
 };
-use color_eyre::Result;
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 
-use super::front_matter::FrontMatter;
+use crate::Error;
 
-#[derive(Debug, Clone, Deserialize)]
-struct SiteConfig {
-    base_url: String,
-}
-
-#[derive(Clone)]
+#[derive(Default, Debug, Clone, Serialize, Deserialize)]
 pub struct Poster {
-    blog_posts: Vec<FrontMatter>,
-    #[allow(dead_code)]
-    agent: BskyAgent,
-    base_url: String,
-    folder: String,
+    bsky_posts: Vec<RecordData>,
 }
 
 impl Poster {
-    pub async fn new<S>(
-        blog_posts: Vec<FrontMatter>,
-        identifier: S,
-        password: S,
-        folder: &str,
-    ) -> Result<Self>
+    pub fn new() -> Result<Self, Error> {
+        Ok(Default::default())
+    }
+
+    pub fn load<P>(&mut self, directory: P) -> Result<&mut Self, Error>
+    where
+        P: AsRef<Path> + Display,
+    {
+        let files = fs::read_dir(directory)?;
+        let mut bsky_posts = Vec::new();
+        for file in files {
+            let file = file?;
+            let file_name = file.file_name().into_string().unwrap();
+            if file_name.ends_with(".post") {
+                let file_path = file.path();
+                let post = fs::File::open(file_path)?;
+                let reader = BufReader::new(post);
+                let bsky_post = serde_json::from_reader(reader)?;
+                bsky_posts.push(bsky_post);
+            }
+        }
+        self.bsky_posts.extend(bsky_posts);
+        Ok(self)
+    }
+
+    pub async fn post_to_bluesky<S>(&self, identifier: S, password: S) -> Result<(), Error>
     where
         S: ToString,
     {
-        // TODO: login to bluesky
-
         if identifier.to_string().is_empty() {
-            return Err(color_eyre::eyre::eyre!("No identifier provided"));
+            return Err(Error::NoBlueskyIdentifier);
         };
 
         if password.to_string().is_empty() {
-            return Err(color_eyre::eyre::eyre!("No password provided"));
+            return Err(Error::NoBlueskyPassword);
         };
 
-        let site_config = fs::read_to_string("config.toml")?;
-
-        let site_config: SiteConfig = toml::from_str(site_config.as_str())?;
-
-        let bsky_config = Config::default();
+        let bsky_config = BskyConfig::default();
 
         let agent = BskyAgent::builder().config(bsky_config).build().await?;
 
         agent
             .login(&identifier.to_string(), &password.to_string())
-            .await?;
+            .await
+            .map_err(|e| Error::BlueskyLoginError(e.to_string()))?;
         // Set labelers from preferences
         let preferences = agent.get_preferences(true).await?;
         agent.configure_labelers_from_preferences(&preferences);
 
         log::info!("Bluesky login successful!");
 
-        Ok(Poster {
-            blog_posts,
-            agent,
-            base_url: site_config.base_url,
-            folder: folder.to_string(),
-        })
-    }
-
-    pub async fn post_to_bluesky(&self) -> Result<()> {
-        // TODO: Create a post
-
-        for blog_post in &self.blog_posts {
-            log::info!("Blog post: {blog_post:#?}");
-
-            log::debug!("Folder: `{}`", self.folder);
-
-            let path = if self.folder.is_empty() {
-                ""
-            } else {
-                &format!("{}/", self.folder)
-            };
-            log::debug!("Path: {path}");
-
-            let post_link = format!(
-                "{}/{}{}/index.html",
-                self.base_url,
-                path,
-                blog_post.filename.as_ref().unwrap()
-            );
-            log::debug!("Post link: {post_link}");
-
-            let post_text = format!(
-                "{}\n\n{} #{}\n\n{}",
-                blog_post.title,
-                blog_post.description,
-                blog_post.taxonomies.tags.join(" #"),
-                post_link
-            );
-
-            log::debug!("Post text: {post_text}");
-
-            let rt = RichText::new_with_detect_facets(&post_text).await?;
-
-            log::debug!("Rich text: {rt:#?}");
-
-            let record_data = RecordData {
-                created_at: Datetime::now(),
-                embed: None,
-                entities: None,
-                facets: rt.facets,
-                labels: None,
-                langs: None,
-                reply: None,
-                tags: None,
-                text: rt.text,
-            };
-
-            log::info!("{:?}", record_data);
-
-            // self.agent.create_record(subject).await?;
+        for post in &self.bsky_posts {
+            log::debug!("Post: {}", post.text.clone());
+            match agent.create_record(post.clone()).await {
+                Ok(res) => {
+                    log::debug!("Post validation status: {:#?}", res.validation_status);
+                    log::info!("Posted to Bluesky!");
+                }
+                Err(e) => {
+                    log::error!("Error posting to Bluesky: {}", e);
+                }
+            }
         }
 
         Ok(())
