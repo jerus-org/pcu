c99f5bf2568a0b331cc8e131b0c8fc2c3d70b18e
Jeremiah Russell <jerry@jrussell.ie>
2025-03-13 17:30:24 +0000

    ✨ feat(cli): enhance CmdPost with commit functionality
    - add Client and GitOps imports to manage git operations
    - modify run method to commit posts sent to Bluesky
    - include sign and commit message for staged posts removal

--- src/cli/bsky/commands/cmd_post.rs
+++ src/cli/bsky/commands/cmd_post.rs
@@ -4,7 +4,7 @@ use clap::Parser;
 use config::Config;
 use poster::Poster;
 
-use crate::{CIExit, Error};
+use crate::{CIExit, Client, Error, GitOps, Sign};
 
 use super::super::BSKY_POSTS_DIR;
 
@@ -12,13 +12,19 @@ use super::super::BSKY_POSTS_DIR;
 pub struct CmdPost;
 
 impl CmdPost {
-    pub async fn run(&self, settings: &Config) -> Result<CIExit, Error> {
+    pub async fn run(&self, client: &Client, settings: &Config) -> Result<CIExit, Error> {
         let id = settings.get::<String>("bsky_id")?;
         let pw = settings.get::<String>("bsky_password")?;
         Poster::new()?
             .load(BSKY_POSTS_DIR)?
             .post_to_bluesky(id, pw)
             .await?;
+
+        // Commit to remove the posts successfully sent to Bluesky
+        let sign = Sign::Gpg;
+        let commit_message = "chore: remove posts that were sent to Bluesky";
+        client.commit_staged(sign, commit_message, "", None)?;
+
         Ok(CIExit::PostedToBluesky)
     }
 }
