07eafce00872e6540fc37bec3010a3a0adfe2b64
Jeremiah Russell <jerry@jrussell.ie>
2025-07-26 08:34:16 +0100

    ✨ feat(bluesky): add hashtag conversion method
    - introduce hashtags method to convert tags into hashtags
    - capitalize first letter of each word and prefix with # if needed

--- src/cli/bsky/commands/cmd_draft/draft/front_matter/bluesky.rs
+++ src/cli/bsky/commands/cmd_draft/draft/front_matter/bluesky.rs
@@ -11,7 +11,32 @@ impl Bluesky {
         self.description.as_deref().unwrap_or("")
     }
 
-    pub fn tags(&self) -> Vec<String> {
+    fn tags(&self) -> Vec<String> {
         self.tags.clone().unwrap_or_default()
+    }
+
+    pub fn hashtags(&self) -> Vec<String> {
+        let mut hashtags = vec![];
+        for tag in &self.tags() {
+            // convert tag to hashtag by capitalising the first letter of each word, removing the spaces and prefixing with a # if required
+            let formatted_tag = tag
+                .split_whitespace()
+                .map(|word| {
+                    let mut chars = word.chars();
+                    match chars.next() {
+                        Some(first) => first.to_uppercase().collect::<String>() + chars.as_str(),
+                        None => String::new(),
+                    }
+                })
+                .collect::<String>();
+            let hashtag = if formatted_tag.starts_with('#') {
+                formatted_tag
+            } else {
+                format!("#{formatted_tag}")
+            };
+            hashtags.push(hashtag);
+        }
+
+        hashtags
     }
 }
