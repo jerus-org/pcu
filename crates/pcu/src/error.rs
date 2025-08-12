562383329c2e4fb046cf2c4c88bcecd342eb0109
Jeremiah Russell <jerry@jrussell.ie>
2025-08-12 16:11:52 +0100

    ♻️ refactor(error): remove unused error variants
    - eliminate unused error variants to simplify error handling
    - remove unnecessary import of env module

--- crates/pcu/src/error.rs
+++ crates/pcu/src/error.rs
@@ -1,5 +1,5 @@
 use regex::Error as RegexError;
-use std::{env, ffi::OsString, fmt::Display, num::ParseIntError};
+use std::{ffi::OsString, fmt::Display, num::ParseIntError};
 use thiserror::Error;
 
 #[derive(Error, Debug)]
@@ -12,21 +12,8 @@ pub enum Error {
     #[error("failed to get output of signing process call: {0}")]
     Stdout(String),
 
-    #[error("bluesky post for `{0}` contains too many characters: {1}")]
-    PostTooManyCharacters(String, usize),
-    #[error("bluesky post for `{0}` contains too many graphemes: {1}")]
-    PostTooManyGraphemes(String, usize),
-
     #[error("{0}")]
     GpgError(String),
-    #[error("No bluesky identifier provided")]
-    NoBlueskyIdentifier,
-    #[error("No bluesky password provided")]
-    NoBlueskyPassword,
-    #[error("Path not found: {0}")]
-    PathNotFound(String),
-    #[error("File extension invalid (must be `{1}`): {0}")]
-    FileExtensionInvalid(String, String),
     #[error("Environment variable PCU_BRANCH not set")]
     EnvVarBranchNotSet,
     #[error("Environment variable specified in PCU_BRANCH not found")]
@@ -51,16 +38,12 @@ pub enum Error {
     DefaultChangeLogNotSet,
     #[error("Invalid path for changelog file {0:?}")]
     InvalidPath(OsString),
-    #[error("Regex string is not valid.")]
-    InvalidRegex,
     #[error("Keep a changelog says: {0}")]
     KeepAChangelog(String),
     #[error("No GitHub API private key found")]
     NoGitHubAPIPrivateKey,
     #[error("No GitHub API Authorisation found")]
     NoGitHubAPIAuth,
-    #[error("On default branch")]
-    OnDefaultBranch,
     #[error("Unknown format for pull request: {0}")]
     UnknownPullRequestFormat(String),
     #[error("No default changelog file found")]
@@ -75,8 +58,6 @@ pub enum Error {
     UrlParse(#[from] url::ParseError),
     #[error("Git2 says: {0:?}")]
     Git2(#[from] git2::Error),
-    #[error("env var says: {0:?}")]
-    EnvVar(#[from] env::VarError),
     #[error("io error says: {0:?}")]
     IO(#[from] std::io::Error),
     #[error("utf8 error says: {0:?}")]
@@ -89,14 +70,6 @@ pub enum Error {
     CargoToml(#[from] cargo_toml::Error),
     #[error("toml deserialization error says: {0:?}")]
     Toml(#[from] toml::de::Error),
-    #[error("bsky_sdk error says: {0:?}")]
-    BskySdk(#[from] bsky_sdk::Error),
-    #[error("bsky_sdk create_session error says: {0:?}")]
-    BlueskyLoginError(String),
-    #[error("serde_json create_session error says: {0:?}")]
-    SerdeJsonError(#[from] serde_json::error::Error),
-    #[error("link-bridge error says: {0:?}")]
-    RedirectorError(#[from] link_bridge::RedirectorError),
     #[error("gen-bsky draft error says: {0:?}")]
     DraftError(#[from] gen_bsky::DraftError),
     #[error("gen-bsky post error says: {0:?}")]
