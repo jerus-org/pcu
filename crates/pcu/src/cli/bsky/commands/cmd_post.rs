18accd9d25cf0279fd75dd29e980a356b8f25027
Jeremiah Russell <jerry@jrussell.ie>
2025-08-13 07:17:34 +0100

    ♻️ refactor(cmd_post): streamline post and delete process
    - integrate post and delete functionality into a single function for efficiency
    - modify commit message to reflect the number of posts deleted
    
    ✨ feat(cmd_post): enhance logging for post actions
    
    - add informative logging when no bluesky posts are found
    - ensure user is informed about CI environment actions

--- crates/pcu/src/cli/bsky/commands/cmd_post.rs
+++ crates/pcu/src/cli/bsky/commands/cmd_post.rs
@@ -1,8 +1,8 @@
-use std::env;
+use std::{env, fmt::Display, path::Path};
 
 use clap::Parser;
 use config::Config;
-use gen_bsky::Post;
+use gen_bsky::{Post, PostError};
 
 use crate::{cli::push::Push, CIExit, Client, Error, GitOps, Sign};
 
@@ -21,9 +21,9 @@ impl CmdPost {
         let id = settings.get::<String>("bsky_id")?;
         let pw = settings.get::<String>("bsky_password")?;
         let store = settings.get::<String>("store")?;
-        let mut poster = Post::new();
-        match poster.load(store) {
-            Ok(_) => {}
+
+        let deleted = match post_and_delete(&id, &pw, &store).await {
+            Ok(d) => d,
             Err(e) => {
                 if self.fail_on_missing {
                     return Err(e.into());
@@ -33,24 +33,50 @@ impl CmdPost {
                 }
             }
         };
-        poster.post_to_bluesky(id, pw).await?;
+
+        if deleted == 0 {
+            log::info!("No bluesky posts found.");
+            return Ok(CIExit::NoFilesToProcess);
+        }
 
         // Commit to remove the posts successfully sent to Bluesky
         let sign = Sign::Gpg;
-        let commit_message = "chore: remove posts that were sent to Bluesky";
+        let commit_message = format!(
+            "chore: remove {} posts that were sent to Bluesky",
+            if deleted == 1 {
+                format!("{deleted} post")
+            } else {
+                format!("{deleted} posts")
+            }
+        );
+
         client
-            .commit_changed_files(sign, commit_message, "", None)
+            .commit_changed_files(sign, &commit_message, "", None)
             .await?;
 
         if env::var("CI").is_ok() && !self.release {
             log::info!("Running in CI, skipping push to remote");
             return Ok(CIExit::DraftedForBluesky);
         }
-        // Push the commit as it only exists until the program exits
+
         Push::new_with(None, false, "v".to_string())
             .run_push()
             .await?;
 
         Ok(CIExit::PostedToBluesky)
     }
+}
+
+async fn post_and_delete<P>(id: &str, pw: &str, store: P) -> Result<usize, PostError>
+where
+    P: AsRef<Path> + Display,
+{
+    let mut poster = Post::new(id, pw)?;
+    let deleted = poster
+        .load(store)?
+        .post_to_bluesky()
+        .await?
+        .delete_posted_posts()?
+        .count_deleted();
+    Ok(deleted)
 }
