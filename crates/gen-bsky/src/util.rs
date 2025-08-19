bc18b2ba81bb8b78156fd95514384bbf7f8840d6
Jeremiah Russell <jerry@jrussell.ie>
2025-08-19 07:39:05 +0100

    ♻️ refactor(util): modularize test utilities
    - move random_name function to test_utils module
    - add get_test_logger and setup_test_environment functions in test_utils module for better test configuration management

--- crates/gen-bsky/src/util.rs
+++ crates/gen-bsky/src/util.rs
@@ -2,15 +2,6 @@ use std::path::{Path, PathBuf};
 
 use crate::{BLOG_DIR, BSKY_DIR, REFERRER_DIR};
 
-#[cfg(test)]
-pub(crate) fn random_name() -> String {
-    use chrono::Utc;
-    let now = Utc::now();
-    let millis = now.timestamp_millis();
-
-    base62::encode(millis as u128)
-}
-
 #[allow(dead_code)]
 fn walk_directory_for_files(path: &Path) -> Vec<PathBuf> {
     let mut files = Vec::new();
@@ -61,4 +52,38 @@ pub fn default_blog_dir() -> PathBuf {
     }
 
     path
+}
+
+#[cfg(test)]
+pub(crate) mod test_utils {
+    use std::str::FromStr;
+
+    use log::LevelFilter;
+    use tempfile::TempDir;
+    use url::Url;
+
+    #[cfg(test)]
+    pub(crate) fn random_name() -> String {
+        use chrono::Utc;
+        let now = Utc::now();
+        let millis = now.timestamp_millis();
+
+        base62::encode(millis as u128)
+    }
+
+    pub(crate) fn get_test_logger(level: LevelFilter) {
+        let mut builder = env_logger::Builder::new();
+        builder.filter(None, level);
+        builder.format_timestamp_secs().format_module_path(false);
+        let _ = builder.try_init();
+    }
+
+    pub(crate) fn setup_test_environment(level: LevelFilter) -> (TempDir, Url) {
+        get_test_logger(level);
+        let temp_dir = tempfile::tempdir().unwrap();
+        log::debug!("Created temp directory: {temp_dir:?}");
+        let base_url = Url::from_str("https://www.example.com/").unwrap();
+
+        (temp_dir, base_url)
+    }
 }
