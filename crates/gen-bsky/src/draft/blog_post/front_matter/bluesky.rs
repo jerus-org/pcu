8dbcfbaf2016b2b7422aae1086fa0910ad1f75d1
Jeremiah Russell <jerry@jrussell.ie>
2025-08-19 07:39:30 +0100

    ♻️ refactor(bluesky): streamline hashtag generation logic
    - move hashtag generation to tags module for cleaner code separation
    - remove redundant code by leveraging existing functionality in tags module

--- crates/gen-bsky/src/draft/blog_post/front_matter/bluesky.rs
+++ crates/gen-bsky/src/draft/blog_post/front_matter/bluesky.rs
@@ -1,5 +1,7 @@
 use serde::{Deserialize, Serialize};
 
+use super::tags;
+
 #[derive(Default, Debug, Clone, Serialize, Deserialize)]
 pub(crate) struct Bluesky {
     description: Option<String>,
@@ -11,33 +13,12 @@ impl Bluesky {
         self.description.as_deref().unwrap_or("")
     }
 
+    #[allow(dead_code)]
     pub(crate) fn tags(&self) -> Vec<String> {
         self.tags.clone().unwrap_or_default()
     }
 
     pub(crate) fn hashtags(&self) -> Vec<String> {
-        let mut hashtags = vec![];
-        for tag in &self.tags() {
-            // convert tag to hashtag by capitalising the first letter of each word,
-            // removing the spaces and prefixing with a # if required
-            let formatted_tag = tag
-                .split_whitespace()
-                .map(|word| {
-                    let mut chars = word.chars();
-                    match chars.next() {
-                        None => String::new(),
-                        Some(first) => first.to_uppercase().collect::<String>() + chars.as_str(),
-                    }
-                })
-                .collect::<String>();
-            let hashtag = if formatted_tag.starts_with('#') {
-                formatted_tag
-            } else {
-                format!("#{formatted_tag}")
-            };
-            hashtags.push(hashtag);
-        }
-
-        hashtags
+        tags::hashtags(self.tags.clone().unwrap_or_default())
     }
 }
